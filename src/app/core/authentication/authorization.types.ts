0b24dc5c7319aecef8eeb367131d7546c02d0e3b
vishnugit007 <55869124+vishnugit007@users.noreply.github.com>
2021-10-21 16:11:15 +0530

    Delete src directory

--- src/app/core/authentication/authorization.types.ts
+++
@@ -1,1 +0,0 @@
-export type AuthGroup = 'VIEW_ONLY' | 'UPDATE_FULL' | 'CREATE';
--- src/app/shared/directives/number-only.directive/number-only.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { NumbersOnlyDirective } from './number-only.directive';
-
-describe('NumberOnlyDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new NumbersOnlyDirective();
-  //   expect(directive).toBeTruthy();
-  // });
-});
